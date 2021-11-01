2438849dd769733c929415ae8e85d7d6e7b9693f
kjt <tae101010@navere.com>
2021-11-01 22:41:04 +0900

    db create table

--- foodAllergy/models.py
+++ foodAllergy/models.py
@@ -2,12 +2,16 @@ from django.db import models
 
 # Create your models here.
 
-class Question(models.Model):
-    subject = models.CharField(max_length=200)
-    content = models.TextField()
+class Result(models.Model):
+    #resultNum = models.AutoField()
+    productName = models.CharField(max_length=100)
+    productContent = models.TextField()
+    allergyResult = models.TextField()
     create_date = models.DateTimeField()
 
-class Answer(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    content = models.TextField()
-    create_date = models.DateTimeField
+class Allergy(models.Model):
+    #allergyNum = models.IntegerField()
+    highLevelAllergy = models.CharField(max_length=30)
+    lowLevelAllergy = models.CharField(max_length=30)
+    level = models.IntegerField()
+    myAllergy = models.CharField(max_length=10)
